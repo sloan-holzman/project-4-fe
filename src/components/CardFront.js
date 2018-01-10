920cbcb23bc490abf9f506dc93b6037dbf1a6596
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-10 15:35:14 -0500

    fix date formatting

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -1,23 +1,20 @@
 import React from 'react'
+import moment from 'moment'
+
 
 const CardFront = ({...props}) => {
   let balance = (props.card.balance && props.card.balance> 0 ? props.card.balance.toFixed(2) : "n/a")
-  let date_options = {
-    year: 'numeric',
-    month: 'short',
-    day: '2-digit'
-  }
   let expiration
   let expiration_class
   if (props.card.expiration) {
-    expiration = new Intl.DateTimeFormat('en-US', date_options).format(Date.parse(props.card.expiration.substring(0,10)))
+    expiration = moment(props.card.expiration.substring(0,10)).format('ll')
     expiration_class = new Date(props.card.expiration) > new Date()  ? "valid" : "expired"
   } else {
     expiration = "n/a"
     expiration_class = "valid"
   }
 
-  let updated = (props.card.updated ? (new Intl.DateTimeFormat('en-US', date_options).format(Date.parse(props.card.updated.substring(0,10)))) : "n/a")
+  let updated = moment(props.card.updated.substring(0,10)).format('ll')
 
   return (
     <div className="card__side">
