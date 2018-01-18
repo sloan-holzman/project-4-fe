890a855ec47d1a2aa5eb900ffde83899782b5fe5
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-18 17:35:28 -0500

    Revert "change to hashRouter from browserRouter"
    This reverts commit 3fb6c228600a700b4974c24b73d92130e5889364.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  HashRouter as Router
+  BrowserRouter as Router
 } from "react-router-dom"
 
 import App from './containers/App';
